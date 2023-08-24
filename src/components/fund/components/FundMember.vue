a4616fbfc1102fe5bb8978355101ba6c580a041c
LEVANTHANHNHAN <t_nhan@brycen.com.vn>
2023-08-24 17:22:32 +0700

    add PayOS

--- src/components/fund/components/FundMember.vue
+++ src/components/fund/components/FundMember.vue
@@ -1,3 +1,9 @@
+<script setup>
+import FundStatusModal from "./FundStatusModal.vue";
+import fundService from "../../../services/fund.service";
+import memberService from "../../../services/member.service";
+</script>
+
 <template>
   <p class="">Quỹ năm {{ new Date().getFullYear() }}</p>
   <div class="row removable">
@@ -5,14 +11,23 @@
       <tbody id="tbody"></tbody>
     </table>
   </div>
+
+  <Teleport to="body">
+    <FundStatusModal
+      :show="isShowModal"
+      @close="isShowModal = false"
+      @update="update"
+    >
+      <template #header> </template>
+    </FundStatusModal>
+  </Teleport>
 </template>
 
 <script>
 import $ from "jquery";
-import fundService from "../../../services/fund.service";
-import memberService from "../../../services/member.service.js";
-import deviceType from "../../../common/device.common.js";
-import dateTime from '../../../common/datetime.common.js'
+import getDeviceType from "../../../common/device.common";
+import dateTime from "../../../common/datetime.common";
+import payOS from "../../../common/payos.common";
 
 export default {
   data() {
@@ -21,14 +36,37 @@ export default {
       deviceType: null,
       curentQuarter: null,
       payType: {
-            NONE: 0,
-            PAID: 1,
-        }
+        NONE: 0,
+        PAID: 1,
+      },
+      isShowModal: false,
     };
   },
   methods: {
+    async createOrderPayOS(cellId) {
+      var orderCode = (await this.getMaxOrderCode()) + 1;
+      var signature = payOS.createSignature(orderCode);
+      console.log(signature);
+    },
+    async getMaxOrderCode() {
+      var orderCode = await fundService
+        .maxOrderCode()
+        .then((response) => {
+          if (response.data.length == 0) {
+            return 0;
+          } else {
+            return response.data[0].order_code;
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+
+      return orderCode;
+    },
+    update() {},
     getMembers() {
-        fundService
+      fundService
         .findFundMember()
         .then((response) => {
           this.members = response.data;
@@ -58,61 +96,137 @@ export default {
 
           // Draw Header Quarters
           if (i == 0 && j < NUMBER_CELL) {
-            td = "<td id=" + i + "-" + j + " class='tg tg-0lax' style='font-weight: bold'>" + quarters[j] + "</td>";
+            td =
+              "<td id=" +
+              i +
+              "-" +
+              j +
+              " class='tg tg-0lax' style='font-weight: bold; background-color: #fff'>" +
+              quarters[j] +
+              "</td>";
           } else {
             // Check member is pay (0: none, 1: paid)
             if (j == dateTime.QuarterType.Q1) {
-                td = this.drawContentCell(dateTime.QuarterType.Q1, this.members[i - 1].quarter1, i, j);
-            }
-            else if (j == dateTime.QuarterType.Q2) {
-                td = this.drawContentCell(dateTime.QuarterType.Q2, this.members[i - 1].quarter2, i, j);
-            }
-            else if (j == dateTime.QuarterType.Q3) {
-                td = this.drawContentCell(dateTime.QuarterType.Q3, this.members[i - 1].quarter3, i, j);
-            }
-            else if (j == dateTime.QuarterType.Q4) {
-                td = this.drawContentCell(dateTime.QuarterType.Q4, this.members[i - 1].quarter4, i, j);
+              td = this.drawContentCell(
+                dateTime.QuarterType.Q1,
+                this.members[i - 1].quarter1,
+                i,
+                j
+              );
+            } else if (j == dateTime.QuarterType.Q2) {
+              td = this.drawContentCell(
+                dateTime.QuarterType.Q2,
+                this.members[i - 1].quarter2,
+                i,
+                j
+              );
+            } else if (j == dateTime.QuarterType.Q3) {
+              td = this.drawContentCell(
+                dateTime.QuarterType.Q3,
+                this.members[i - 1].quarter3,
+                i,
+                j
+              );
+            } else if (j == dateTime.QuarterType.Q4) {
+              td = this.drawContentCell(
+                dateTime.QuarterType.Q4,
+                this.members[i - 1].quarter4,
+                i,
+                j
+              );
             }
           }
 
           // Draw Member Name
           if (i > 0 && j == 0) {
-            var memberName = this.deviceType == "desktop" ? this.members[i - 1].member_name : this.members[i - 1].sort_name
-            td = "<td id=" + i + "-" + j + " class='tg tg-0lax' style='width: 200px; font-weight: bold'>" + memberName + "</td>";
+            var memberName =
+              this.deviceType == "desktop"
+                ? this.members[i - 1].member_name
+                : this.members[i - 1].sort_name;
+            td =
+              "<td id=" +
+              i +
+              "-" +
+              j +
+              " class='tg tg-0lax' style='width: 200px; font-weight: bold; background-color: #fff'>" +
+              memberName +
+              "</td>";
           }
 
           $("#tr" + i).append(td);
         }
       }
+
+      // Add event cell click
+      var self = this;
+      $("#table td").click(function () {
+        var cellId = $(this).attr("id");
+        var cellClass = $(this).attr("class");
+
+        // If click cell empty is not show Modal
+        if (!cellId.includes("0") && !cellClass.includes("none")) {
+          self.createOrderPayOS(cellId);
+          //self.isShowModal = true;
+        }
+      });
     },
     drawContentCell(type, value, i, j) {
-        var td = "<td id=" + i + "-" + j + " class='tg tg-0lax error'></td>";
+      var td =
+        "<td id=" +
+        this.members[i - 1].member_id +
+        "-" +
+        j +
+        "-" +
+        this.members[i - 1].year +
+        " class='tg tg-0lax tg-content error'></td>";
 
-        // Check if current quarter is small Quarter1, 2, 3, 4
-        if (this.curentQuarter < type) {
-            td = "<td id=" + i + "-" + j + " class='tg tg-0lax'></td>";
+      // Check if current quarter is small Quarter1, 2, 3, 4
+      if (this.curentQuarter < type) {
+        td =
+          "<td id=" +
+          this.members[i - 1].member_id +
+          "-" +
+          j +
+          "-" +
+          this.members[i - 1].year +
+          " class='tg tg-0lax none'></td>";
+      } else {
+        // Check member is pay (0: none, 1: paid)
+        if (value == this.payType.NONE) {
+          td =
+            "<td id=" +
+            this.members[i - 1].member_id +
+            "-" +
+            j +
+            "-" +
+            this.members[i - 1].year +
+            " class='tg tg-0lax tg-content error'></td>";
+        } else {
+          td =
+            "<td id=" +
+            this.members[i - 1].member_id +
+            "-" +
+            j +
+            "-" +
+            this.members[i - 1].year +
+            " class='tg tg-0lax tg-content success'></td>";
         }
-        else {
-            // Check member is pay (0: none, 1: paid)
-            if (value == this.payType.NONE) {
-                td = "<td id=" + i + "-" + j + " class='tg tg-0lax error'></td>";
-            }
-            else {
-                td = "<td id=" + i + "-" + j + " class='tg tg-0lax success'></td>";
-            }
-        }
+      }
 
-        return td;
+      return td;
     },
   },
   mounted() {
     this.getMembers();
-    this.deviceType = deviceType;
+    this.deviceType = getDeviceType;
     this.curentQuarter = dateTime.getQuarter;
   },
 };
 </script>
 <style>
+table {
+  box-shadow: 0 20px 27px 0 rgba(0, 0, 0, 0.05);
+}
 .tg .tg-0lax {
   text-align: center;
   vertical-align: top;
@@ -120,13 +234,16 @@ export default {
   background-size: 34px 34px;
   background-position: center;
 }
+.tg-content {
+  cursor: pointer;
+}
 .success {
   background-color: #dffff4;
   background-image: url("https://img.icons8.com/dotty/80/12B886/ok.png");
 }
 .error {
-    background-image: url("https://img.icons8.com/dotty/80/FA5252/cancel.png");
-    background-color: #fdebeb;
+  background-image: url("https://img.icons8.com/dotty/80/FA5252/cancel.png");
+  background-color: #fdebeb;
 }
 .tg td {
   font-size: 15px;
